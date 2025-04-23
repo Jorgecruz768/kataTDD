e2d40b97fca4b9a041b8a6ed4ad562009860d3e3
Jorgecruz768 <74361793@continental.edu.pe>
2025-04-23 11:38:20 -0500

    Caso 1: Calcular promedio con conjunto vacío (Rojo)

--- test/TestConjunto.py
+++ test/TestConjunto.py
@@ -2,22 +2,7 @@ import unittest
 from src.logica.Conjunto import Conjunto
 
 class TestConjunto(unittest.TestCase):
-
-    def test_promedio_lista_vacia(self):
+    def test_conjunto_vacio_retornaNone(self):
         conjunto = Conjunto([])
         self.assertIsNone(conjunto.promedio())
 
-    def test_promedio_un_elemento(self):
-        conjunto = Conjunto([10])
-        self.assertEqual(conjunto.promedio(), 10)
-
-    def test_promedio_dos_elementos(self):
-        conjunto = Conjunto([10, 20])
-        self.assertEqual(conjunto.promedio(), 15)
-
-    def test_promedio_varios_elementos(self):
-        conjunto = Conjunto([10, 20, 30, 40])
-        self.assertEqual(conjunto.promedio(), 25)
-
-if __name__ == '__main__':
-    unittest.main()
